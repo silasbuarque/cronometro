c7db4953ccd0107c54d1f2838af3bc2eef996f96
silasbuarque <silasbuarque@gmail.com>
2021-03-15 08:49:04 -0300

    Subindo os arquivos

---
+++ Estilos/style.js
@@ -0,0 +1,21 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+    container:{
+        justifyContent:'center',
+        alignItems:'center', 
+        backgroundColor:'#D3D3D3', 
+    },
+    reposta:{
+        justifyContent:'center',
+        alignItems:'center',
+        flex:1,
+    },
+    caixas:{
+        borderWidth:1, 
+        width: 120, 
+    },
+    textocaixa:{
+        fontSize:20,
+    },
+})
